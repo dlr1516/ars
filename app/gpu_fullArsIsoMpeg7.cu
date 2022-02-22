dcf79fdf4b90c8e6c1635e8c00ced95dfcc7494d
RIMLab <ernesto.fontana96@gmail.com>
2022-02-22 16:56:25 +0100

    Ran first (unoptimized) test

--- app/gpu_fullArsIsoMpeg7.cu
+++ app/gpu_fullArsIsoMpeg7.cu
@@ -42,6 +42,7 @@ struct TestParams {
     int arsIsoOrder;
     double arsIsoSigma;
     double arsIsoThetaToll;
+    cuars::ArsKernelIsotropic2d::ComputeMode arsIsoPnebiMode;
 
 
     bool extrainfoEnable;
@@ -143,6 +144,8 @@ int main(int argc, char **argv) {
     params.getParam<double>("arsisoSigma", tparams.arsIsoSigma, 1.0);
     params.getParam<double>("arsisoTollDeg", tparams.arsIsoThetaToll, 0.5);
     tparams.arsIsoThetaToll *= M_PI / 180.0;
+    //    params.getParam<unsigned int>("arsisoPnebiMode", tparams.arsIsoPnebiMode, cuars::ArsKernelIsotropic2d::ComputeMode::PNEBI_DOWNWARD);
+
 
     arsSrc.setARSFOrder(tparams.arsIsoOrder);
     //    arsSrc.initLUT(0.0001);
@@ -235,6 +238,7 @@ int main(int argc, char **argv) {
 
     //execution couple-by-couple (of files) of ARS
     int countPairs = 0;
+    const int maxNumPtsAllowed = 4095;
     for (auto& comp : outPairs) {
         if (countPairs % tparams.fileSkipper) {
             countPairs++;
@@ -247,8 +251,25 @@ int main(int argc, char **argv) {
 
         std::cout << "[" << countPairs << "/" << outPairs.size() << "]\n" << "  * \"" << inputFilenames[comp.first] << "\"\n    \"" << inputFilenames[comp.second] << "\"" << std::endl;
         rotTrue = pointsDst.getRotTheta() - pointsSrc.getRotTheta();
-        
+
         setupParallelization(paiParams, pointsSrc.points().size(), pointsDst.points().size(), tparams.arsIsoOrder);
+        if (paiParams.numPts > maxNumPtsAllowed) {
+            std::cout << "CANNOT process all data in single block... skipping to next" << std::endl;
+            outfile
+                    << std::setw(20) << getShortName(inputFilenames[comp.first]) << " "
+                    << std::setw(6) << pointsSrc.getNumIn() << " "
+                    << std::fixed << std::setprecision(1) << std::setw(6) << pointsSrc.getNoiseSigma() << " "
+                    << std::setw(6) << pointsSrc.getNumOccl() << " "
+                    << std::setw(6) << pointsSrc.getNumRand() << " "
+                    << std::setw(20) << getShortName(inputFilenames[comp.second]) << " "
+                    << std::setw(6) << pointsSrc.getNumIn() << " "
+                    << std::fixed << std::setprecision(1) << std::setw(6) << pointsDst.getNoiseSigma() << " "
+                    << std::setw(6) << pointsDst.getNumOccl() << " "
+                    << std::setw(6) << pointsDst.getNumRand() << " "
+                    << "SKIPPED" << std::endl;
+            continue;
+        }
+
 
         //    if (rotTrue < 0.0) rotTrue += M_PI;
         //    else if (rotTrue > M_PI) rotTrue -= M_PI;
@@ -302,6 +323,7 @@ int main(int argc, char **argv) {
         std::cout << "\n\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n";
     }
 
+    //    std::cout << "maxNumPts " << maxNumPts << " maxNumPtsPadded " << maxNumPtsPadded << std::endl;
 
     outfile.close();
 
@@ -498,171 +520,173 @@ void setupParallelization(ParlArsIsoParams& pp, int pointsSrcSz, int pointsDstSz
 }
 
 void gpu_estimateRotationArsIso(const ArsImgTests::PointReaderWriter& pointsSrc, const ArsImgTests::PointReaderWriter& pointsDst, TestParams& tp, ParlArsIsoParams& paip, double& rotOut) {
-    //    //ARS SRC -> preparation for kernel calls and kernel calls
-    //    cudaEvent_t startSrc, stopSrc; //timing using CUDA events
-    //    cudaEventCreate(&startSrc);
-    //    cudaEventCreate(&stopSrc);
-    //    cuars::Vec2d * kernelInputSrc;
-    //    cudaMalloc((void**) &kernelInputSrc, numPtsAfterPadding * sizeof (cuars::Vec2d));
-    //    cudaMemcpy(kernelInputSrc, pointsSrc.points().data(), numPtsAfterPadding * sizeof (cuars::Vec2d), cudaMemcpyHostToDevice);
-    //
-    //    double *coeffsMatSrc;
-    //    cudaMalloc((void**) &coeffsMatSrc, coeffsMatTotalSz * sizeof (double));
-    //    cudaMemset(coeffsMatSrc, 0.0, coeffsMatTotalSz * sizeof (double));
-    //    //    for (int i = 0; i < coeffsMatTotalSz; ++i) {
-    //    //        coeffsMaSrc1[i] = 0.0;
-    //    //    }
-    //    double* d_coeffsArsSrc;
-    //    cudaMalloc((void**) &d_coeffsArsSrc, coeffsMatNumColsPadded * sizeof (double));
-    //    cudaMemset(d_coeffsArsSrc, 0.0, coeffsMatNumColsPadded * sizeof (double));
-    //
-    //    cudaEventRecord(startSrc);
-    //    iigKernelDownward << <numBlocks, blockSize >> >(kernelInputSrc, arsSigma, arsSigma, numPts, numPtsAfterPadding, arsOrder, coeffsMatNumColsPadded, pnebiMode, coeffsMatSrc);
-    //    sumColumns << <1, sumBlockSz>> >(coeffsMatSrc, numPtsAfterPadding, coeffsMatNumColsPadded, d_coeffsArsSrc);
-    //    cudaEventRecord(stopSrc);
-    //
-    //    double* coeffsArsSrc = new double [coeffsMatNumColsPadded];
-    //    cudaMemcpy(coeffsArsSrc, d_coeffsArsSrc, coeffsMatNumColsPadded * sizeof (double), cudaMemcpyDeviceToHost);
-    //
-    //    cudaEventSynchronize(stopSrc);
-    //    float millisecondsSrc = 0.0f;
-    //    cudaEventElapsedTime(&millisecondsSrc, startSrc, stopSrc);
-    //    std::cout << "SRC -> insertIsotropicGaussians() " << millisecondsSrc << " ms" << std::endl;
-    //
-    //    cudaError_t cudaerr = cudaDeviceSynchronize();
-    //    if (cudaerr != cudaSuccess)
-    //        printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
-    //
-    //    //    for (int i = 0; i < coeffsMatNumColsPadded; ++i) {
-    //    //        std::cout << "coeffsArsSrc[" << i << "] " << coeffsArsSrc[i] << std::endl;
-    //    //    }
-    //
-    //    cudaFree(coeffsMatSrc);
-    //    cudaFree(kernelInputSrc);
-    //    cudaFree(d_coeffsArsSrc);
-    //    cudaEventDestroy(startSrc);
-    //    cudaEventDestroy(stopSrc);
-    //    //END OF ARS SRC
-    //
-    //
-    //
-    //    std::cout << "\n------\n" << std::endl; //"pause" between ars src and ars dst
-    //
-    //
-    //
-    //    //ARS DST -> preparation for kernel calls and kernel calls
-    //    cudaEvent_t startDst, stopDst; //timing using CUDA events
-    //    cudaEventCreate(&startDst);
-    //    cudaEventCreate(&stopDst);
-    //    cuars::Vec2d *kernelInputDst;
-    //    cudaMalloc((void**) &kernelInputDst, numPtsAfterPadding * sizeof (cuars::Vec2d));
-    //    cudaMemcpy(kernelInputDst, pointsDst.points().data(), numPtsAfterPadding * sizeof (cuars::Vec2d), cudaMemcpyHostToDevice);
-    //
-    //    double *coeffsMatDst; //magari evitare di fare il delete e poi riallocarla è più efficiente (anche se comunque ci sarebbe poi da settare tutto a 0)
-    //    cudaMalloc((void**) &coeffsMatDst, coeffsMatTotalSz * sizeof (double));
-    //    cudaMemset(coeffsMatDst, 0.0, coeffsMatTotalSz * sizeof (double));
-    //    //    for (int i = 0; i < coeffsMatTotalSz; ++i) {
-    //    //        coeffsMatDst[i] = 0.0;
-    //    //    }
-    //    double* d_coeffsArsDst;
-    //    cudaMalloc((void**) &d_coeffsArsDst, coeffsMatNumColsPadded * sizeof (double));
-    //    cudaMemset(d_coeffsArsDst, 0.0, coeffsMatNumColsPadded * sizeof (double));
-    //
-    //    cudaEventRecord(startDst);
-    //    iigKernelDownward << <numBlocks, blockSize >> >(kernelInputDst, arsSigma, arsSigma, numPts, numPtsAfterPadding, arsOrder, coeffsMatNumColsPadded, pnebiMode, coeffsMatDst);
-    //    sumColumns << <1, sumBlockSz>> >(coeffsMatDst, numPtsAfterPadding, coeffsMatNumColsPadded, d_coeffsArsDst);
-    //    cudaEventRecord(stopDst);
-    //
-    //
-    //
-    //    double* coeffsArsDst = new double [coeffsMatNumColsPadded];
-    //    cudaMemcpy(coeffsArsDst, d_coeffsArsDst, coeffsMatNumColsPadded * sizeof (double), cudaMemcpyDeviceToHost);
-    //
-    //    cudaEventSynchronize(stopDst);
-    //    float millisecondsDst = 0.0f;
-    //    cudaEventElapsedTime(&millisecondsDst, startDst, stopDst);
-    //    std::cout << "DST -> insertIsotropicGaussiansDst() " << millisecondsDst << " ms" << std::endl;
-    //
-    //    cudaerr = cudaDeviceSynchronize();
-    //    if (cudaerr != cudaSuccess)
-    //        printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
-    //
-    //    //    for (int i = 0; i < coeffsMatNumColsPadded; ++i) {
-    //    //        std::cout << "coeffsArsDst[" << i << "] " << coeffsArsDst[i] << std::endl;
-    //    //    }
-    //
-    //    cudaFree(coeffsMatDst);
-    //    cudaFree(kernelInputDst);
-    //    cudaFree(d_coeffsArsDst);
-    //    cudaEventDestroy(startDst);
-    //    cudaEventDestroy(stopDst);
-    //    //END OF ARS DST
-    //
-    //
-    //
-    //
-    //
-    //    //Computation final computations (correlation, ...) on CPU
+    //ARS SRC -> preparation for kernel calls and kernel calls
+    cudaEvent_t startSrc, stopSrc; //timing using CUDA events
+    cudaEventCreate(&startSrc);
+    cudaEventCreate(&stopSrc);
+    cuars::Vec2d * kernelInputSrc;
+    cudaMalloc((void**) &kernelInputSrc, paip.numPtsAfterPadding * sizeof (cuars::Vec2d));
+    cudaMemcpy(kernelInputSrc, pointsSrc.points().data(), paip.numPtsAfterPadding * sizeof (cuars::Vec2d), cudaMemcpyHostToDevice);
+
+    double *coeffsMatSrc;
+    cudaMalloc((void**) &coeffsMatSrc, paip.coeffsMatTotalSz * sizeof (double));
+    cudaMemset(coeffsMatSrc, 0.0, paip.coeffsMatTotalSz * sizeof (double));
+    //    for (int i = 0; i < coeffsMatTotalSz; ++i) {
+    //        coeffsMaSrc1[i] = 0.0;
+    //    }
+    double* d_coeffsArsSrc;
+    cudaMalloc((void**) &d_coeffsArsSrc, paip.coeffsMatNumColsPadded * sizeof (double));
+    cudaMemset(d_coeffsArsSrc, 0.0, paip.coeffsMatNumColsPadded * sizeof (double));
+
+    cudaEventRecord(startSrc);
+    iigKernelDownward << <paip.numBlocks, paip.blockSize >> >(kernelInputSrc, tp.arsIsoSigma, tp.arsIsoSigma, paip.numPts, paip.numPtsAfterPadding, tp.arsIsoOrder, paip.coeffsMatNumColsPadded, tp.arsIsoPnebiMode, coeffsMatSrc);
+    sumColumns << <1, paip.sumBlockSz>> >(coeffsMatSrc, paip.numPtsAfterPadding, paip.coeffsMatNumColsPadded, d_coeffsArsSrc);
+    cudaEventRecord(stopSrc);
+
+    double* coeffsArsSrc = new double [paip.coeffsMatNumColsPadded];
+    cudaMemcpy(coeffsArsSrc, d_coeffsArsSrc, paip.coeffsMatNumColsPadded * sizeof (double), cudaMemcpyDeviceToHost);
+
+    cudaEventSynchronize(stopSrc);
+    float millisecondsSrc = 0.0f;
+    cudaEventElapsedTime(&millisecondsSrc, startSrc, stopSrc);
+    std::cout << "SRC -> insertIsotropicGaussians() " << millisecondsSrc << " ms" << std::endl;
+
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
+
+    //    for (int i = 0; i < coeffsMatNumColsPadded; ++i) {
+    //        std::cout << "coeffsArsSrc[" << i << "] " << coeffsArsSrc[i] << std::endl;
+    //    }
+
+    cudaFree(coeffsMatSrc);
+    cudaFree(kernelInputSrc);
+    cudaFree(d_coeffsArsSrc);
+    cudaEventDestroy(startSrc);
+    cudaEventDestroy(stopSrc);
+    //END OF ARS SRC
+
+
+
+    std::cout << "\n------\n" << std::endl; //"pause" between ars src and ars dst
+
+
+
+    //ARS DST -> preparation for kernel calls and kernel calls
+    cudaEvent_t startDst, stopDst; //timing using CUDA events
+    cudaEventCreate(&startDst);
+    cudaEventCreate(&stopDst);
+    cuars::Vec2d *kernelInputDst;
+    cudaMalloc((void**) &kernelInputDst, paip.numPtsAfterPadding * sizeof (cuars::Vec2d));
+    cudaMemcpy(kernelInputDst, pointsDst.points().data(), paip.numPtsAfterPadding * sizeof (cuars::Vec2d), cudaMemcpyHostToDevice);
+
+    double *coeffsMatDst; //magari evitare di fare il delete e poi riallocarla è più efficiente (anche se comunque ci sarebbe poi da settare tutto a 0)
+    cudaMalloc((void**) &coeffsMatDst, paip.coeffsMatTotalSz * sizeof (double));
+    cudaMemset(coeffsMatDst, 0.0, paip.coeffsMatTotalSz * sizeof (double));
+    //    for (int i = 0; i < coeffsMatTotalSz; ++i) {
+    //        coeffsMatDst[i] = 0.0;
+    //    }
+    double* d_coeffsArsDst;
+    cudaMalloc((void**) &d_coeffsArsDst, paip.coeffsMatNumColsPadded * sizeof (double));
+    cudaMemset(d_coeffsArsDst, 0.0, paip.coeffsMatNumColsPadded * sizeof (double));
+
+    cudaEventRecord(startDst);
+    iigKernelDownward << <paip.numBlocks, paip.blockSize >> >(kernelInputDst, tp.arsIsoSigma, tp.arsIsoSigma, paip.numPts, paip.numPtsAfterPadding, tp.arsIsoOrder, paip.coeffsMatNumColsPadded, tp.arsIsoPnebiMode, coeffsMatDst);
+    sumColumns << <1, paip.sumBlockSz>> >(coeffsMatDst, paip.numPtsAfterPadding, paip.coeffsMatNumColsPadded, d_coeffsArsDst);
+    cudaEventRecord(stopDst);
+
+
+
+    double* coeffsArsDst = new double [paip.coeffsMatNumColsPadded];
+    cudaMemcpy(coeffsArsDst, d_coeffsArsDst, paip.coeffsMatNumColsPadded * sizeof (double), cudaMemcpyDeviceToHost);
+
+    cudaEventSynchronize(stopDst);
+    float millisecondsDst = 0.0f;
+    cudaEventElapsedTime(&millisecondsDst, startDst, stopDst);
+    std::cout << "DST -> insertIsotropicGaussiansDst() " << millisecondsDst << " ms" << std::endl;
+
+    cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
+
+    //    for (int i = 0; i < coeffsMatNumColsPadded; ++i) {
+    //        std::cout << "coeffsArsDst[" << i << "] " << coeffsArsDst[i] << std::endl;
+    //    }
+
+    cudaFree(coeffsMatDst);
+    cudaFree(kernelInputDst);
+    cudaFree(d_coeffsArsDst);
+    cudaEventDestroy(startDst);
+    cudaEventDestroy(stopDst);
+    //END OF ARS DST
+
+
+
+
+
+    //Computation final computations (correlation, ...) on CPU
     //    std::cout << "\nARS Coefficients:\n";
     //    std::cout << "Coefficients: Src, Dst, Cor" << std::endl;
-    //
-    //    double thetaMax, corrMax, fourierTol;
-    //    fourierTol = 1.0; // TODO: check for a proper tolerance
-    //
-    //    std::vector<double> coeffsCor;
-    //    {
-    //        cuars::ScopedTimer("ars.correlation()");
-    //        std::vector<double> tmpSrc;
-    //        tmpSrc.assign(coeffsArsSrc, coeffsArsSrc + coeffsMatNumColsPadded);
-    //        std::vector<double> tmpDst;
-    //        tmpDst.assign(coeffsArsDst, coeffsArsDst + coeffsMatNumColsPadded);
-    //        cuars::computeFourierCorr(tmpSrc, tmpDst, coeffsCor);
-    //        cuars::findGlobalMaxBBFourier(coeffsCor, 0.0, M_PI, arsThetaToll, fourierTol, thetaMax, corrMax);
-    //        rotArs = thetaMax;
-    //    }
-    //
-    //
-    //
-    //    arsSrc.setCoefficients(coeffsArsSrc, coeffsMatNumCols);
+
+    double thetaMax, corrMax, fourierTol;
+    fourierTol = 1.0; // TODO: check for a proper tolerance
+
+    std::vector<double> coeffsCor;
+    {
+        cuars::ScopedTimer("ars.correlation()");
+        std::vector<double> tmpSrc;
+        tmpSrc.assign(coeffsArsSrc, coeffsArsSrc + paip.coeffsMatNumColsPadded);
+        std::vector<double> tmpDst;
+        tmpDst.assign(coeffsArsDst, coeffsArsDst + paip.coeffsMatNumColsPadded);
+        cuars::computeFourierCorr(tmpSrc, tmpDst, coeffsCor);
+        cuars::findGlobalMaxBBFourier(coeffsCor, 0.0, M_PI, tp.arsIsoThetaToll, fourierTol, thetaMax, corrMax);
+        rotOut = thetaMax; //!! rotOut is passed to the function as reference
+    }
+
+
+    //  Output coeffs check: CPU version
+    //    arsSrc.setCoefficients(coeffsArsSrc, paip.coeffsMatNumCols);
     //    //    for (int i = 0; i < coeffsVectorMaxSz; i++) {
     //    //        std::cout << "arsSrc - coeff_d[" << i << "] " << d_coeffsMat1[i] << std::endl;
     //    //    }
-    //    arsDst.setCoefficients(coeffsArsDst, coeffsMatNumCols);
+    //    arsDst.setCoefficients(coeffsArsDst, paip.coeffsMatNumCols);
     //    for (int i = 0; i < arsSrc.coefficients().size() && i < arsDst.coefficients().size(); ++i) {
     //        std::cout << "\t" << i << " \t" << arsSrc.coefficients().at(i) << " \t" << arsDst.coefficients().at(i) << " \t" << coeffsCor[i] << std::endl;
     //    }
+
+    //  Output coeffs check: GPU version
+    //    for (int i = 0; i < paip.coeffsMatNumCols; ++i) {
+    //        std::cout << "\t" << i << " \t" << coeffsArsSrc[i] << " \t" << coeffsArsDst[i] << " \t" << coeffsCor[i] << std::endl;
+    //    }
     //    std::cout << std::endl;
-    //
-    //
-    //
-    //    // Computes the rotated points,centroid, affine transf matrix between src and dst
-    //    ArsImgTests::PointReaderWriter pointsRot(pointsSrc.points());
-    //    cuars::Vec2d centroidSrc = pointsSrc.computeCentroid();
-    //    cuars::Vec2d centroidDst = pointsDst.computeCentroid();
-    //    cuars::Affine2d rotSrcDst = ArsImgTests::PointReaderWriter::coordToTransform(0.0, 0.0, rotArs);
-    //    //    cuars::Vec2d translSrcDst = centroidDst - rotSrcDst * centroidSrc;
-    //    cuars::Vec2d translSrcDst;
-    //    cuars::vec2diff(translSrcDst, centroidDst, cuars::aff2TimesVec2WRV(rotSrcDst, centroidSrc));
-    //    //    std::cout << "centroidSrc " << centroidSrc.transpose() << "\n"
-    //    //            << "rotSrcDst\n" << rotSrcDst.matrix() << "\n"
-    //    //            << "translation: [" << translSrcDst.transpose() << "] rotation[deg] " << (180.0 / M_PI * rotArs) << "\n";
+
+
+
+    // Computes the rotated points,centroid, affine transf matrix between src and dst
+    ArsImgTests::PointReaderWriter pointsRot(pointsSrc.points());
+    cuars::Vec2d centroidSrc = pointsSrc.computeCentroid();
+    cuars::Vec2d centroidDst = pointsDst.computeCentroid();
+    cuars::Affine2d rotSrcDst = ArsImgTests::PointReaderWriter::coordToTransform(0.0, 0.0, rotOut);
+    //    cuars::Vec2d translSrcDst = centroidDst - rotSrcDst * centroidSrc;
+    cuars::Vec2d translSrcDst;
+    cuars::vec2diff(translSrcDst, centroidDst, cuars::aff2TimesVec2WRV(rotSrcDst, centroidSrc));
     //    std::cout << "centroidSrc " << centroidSrc.x << " \t" << centroidSrc.y << "\n"
     //            << "centroidDst " << centroidDst.x << " \t" << centroidDst.y << "\n"
     //            << "rotSrcDst\n" << rotSrcDst << "\n"
-    //            << "translation: [" << translSrcDst.x << " \t" << translSrcDst.y << "] rotation[deg] " << (180.0 / M_PI * rotArs) << "\n";
-    //    pointsRot.applyTransform(translSrcDst.x, translSrcDst.y, rotArs);
-    //
-    //
-    //
-    //    rotTrue = pointsDst.getRotTheta() - pointsSrc.getRotTheta();
+    //            << "translation: [" << translSrcDst.x << " \t" << translSrcDst.y << "] rotation[deg] " << (180.0 / M_PI * rotOut) << "\n";
+    pointsRot.applyTransform(translSrcDst.x, translSrcDst.y, rotOut);
+
+
+
+    //    double rotTrue = pointsDst.getRotTheta() - pointsSrc.getRotTheta();
     //    std::cout << "\n***\npointsDst.getrotTheta() [deg]" << (180 / M_PI * pointsDst.getRotTheta())
     //            << ", pointsSrc.getrotTheta() [deg] " << (180.0 / M_PI * pointsSrc.getRotTheta()) << "\n";
     //    std::cout << "rotTrue[deg] \t" << (180.0 / M_PI * rotTrue) << " \t" << (180.0 / M_PI * mod180(rotTrue)) << std::endl;
-    //    std::cout << "rotArs[deg] \t" << (180.0 / M_PI * rotArs) << " \t" << (180.0 / M_PI * mod180(rotArs)) << std::endl;
-    //
-    //    //Free CPU memory
-    //    free(coeffsArsSrc);
-    //    free(coeffsArsDst);
+    //    std::cout << "rotArs[deg] \t" << (180.0 / M_PI * rotOut) << " \t" << (180.0 / M_PI * mod180(rotOut)) << std::endl;
+
+    //Free CPU memory
+    free(coeffsArsSrc);
+    free(coeffsArsDst);
 }
 
 double mod180(double angle) {
