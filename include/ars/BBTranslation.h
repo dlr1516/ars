2a7554e0425cda2b6d11d72ff91a985a73ca4034
ErnestF22 <ernesto.fontana96@gmail.com>
2023-03-12 18:10:46 +0100

    Added explicit 'inline' to some functions

--- include/ars/BBTranslation.h
+++ include/ars/BBTranslation.h
@@ -11,9 +11,30 @@
 namespace ars
 {
 
-    double distancePointBox(const Vector2 &p,
+    inline double distancePointBox(const Vector2 &p,
                             const Vector2 &boxMin,
-                            const Vector2 &boxMax);
+                            const Vector2 &boxMax)
+    {
+        double dist = 0.0;
+        double len;
+        for (int d = 0; d < 2; ++d)
+        {
+            if (boxMin(d) <= p(d) && p(d) <= boxMax(d))
+            {
+                len = 0.0;
+            }
+            else if (p(d) < boxMin(d))
+            {
+                len = boxMin(d) - p(d);
+            }
+            else
+            {
+                len = p(d) - boxMax(d);
+            }
+            dist += len * len;
+        }
+        return dist;
+    }
 
     struct Box
     {
@@ -23,24 +44,107 @@ namespace ars
         double upper_;
         double eps_;
 
-        Box(const Vector2 &min, const Vector2 &max, const double eps);
+        Box(const Vector2 &min, const Vector2 &max, const double eps)
+            : min_(min), max_(max), lower_(0.0), upper_(0.0), eps_(eps) {}
 
         Box(const Vector2 &min,
-            const Vector2 &max,
-            const VectorVector2 &ptsSrc,
-            const VectorVector2 &ptsDst,
-            const double eps);
+                 const Vector2 &max,
+                 const VectorVector2 &ptsSrc,
+                 const VectorVector2 &ptsDst,
+                 const double eps)
+        {
+            double dist, distMin, distUpper, distUpperMin;
+            Vector2 boxMin, boxMax, boxMid;
+            min_ = min;
+            max_ = max;
+            eps_ = eps;
+            // computeBoundsNaive(ptsSrc, ptsDst);
+            computeBoundsInlier(ptsSrc, ptsDst);
+        }
 
-        virtual ~Box();
+        ~Box() {}
 
         void computeBoundsNaive(const VectorVector2 &ptsSrc,
-                                const VectorVector2 &ptsDst);
+                                     const VectorVector2 &ptsDst)
+        {
+            double distLower, distLowerMin, distUpper, distUpperMin;
+            Vector2 boxMin, boxMax, boxMid;
+            lower_ = 0.0;
+            upper_ = 0.0;
+            for (int is = 0; is < ptsSrc.size(); ++is)
+            {
+                distLowerMin = 1e+6;
+                distUpperMin = 1e+6;
+                boxMin = ptsSrc[is] + min_;
+                boxMax = ptsSrc[is] + max_;
+                boxMid = ptsSrc[is] + 0.5 * (max_ + min_);
+                for (int id = 0; id < ptsDst.size(); ++id)
+                {
+                    distLower = distancePointBox(ptsDst[id], boxMin, boxMax);
+                    if (distLower < distLowerMin)
+                    {
+                        distLowerMin = distLower;
+                    }
+                    distUpper = (ptsDst[id] - (ptsSrc[is] + boxMid)).squaredNorm();
+                    // ARS_VAR5(boxMid.transpose(), ptsSrc[is].transpose(),
+                    //          ptsDst[id].transpose(), distUpper, distUpperMin);
+                    if (distUpper < distUpperMin)
+                    {
+                        distUpperMin = distUpper;
+                    }
+                }
+                lower_ += distLowerMin;
+                upper_ += distUpperMin;
+                // ARS_VAR4(distLowerMin, distUpperMin, lower_, upper_);
+            }
+        }
 
         void computeBoundsInlier(const VectorVector2 &ptsSrc,
-                                 const VectorVector2 &ptsDst);
+                                      const VectorVector2 &ptsDst)
+        {
+            Vector2 mid = 0.5 * (min_ + max_);
+            Vector2 srcTransl;
+            double dist, len;
+            bool inlierFoundUpper, inlierFoundLower;
+
+            len = 0.5 * (max_ - min_).maxCoeff(); // Half of Infinity norm
+            lower_ = (double)ptsSrc.size();
+            upper_ = (double)ptsSrc.size();
+            ARS_VAR4(lower_, upper_, len, mid.transpose());
+            for (int is = 0; is < ptsSrc.size(); ++is)
+            {
+                srcTransl = ptsSrc[is] + mid;
+                inlierFoundLower = false;
+                inlierFoundUpper = false;
+                // ARS_VAR1(srcTransl.transpose());
+                for (int id = 0; id < ptsDst.size() && !(inlierFoundLower && inlierFoundUpper); ++id)
+                {
+                    // dist = (ptsDst[id] - srcTransl).norm();
+                    dist = (ptsDst[id] - srcTransl).cwiseAbs().maxCoeff(); // Infinity norm
+                    // ARS_VAR4(ptsDst[id].transpose(), dist, dist < eps_, dist < eps_ + len);
+                    if (dist < eps_)
+                    {
+                        inlierFoundUpper = true;
+                    }
+                    if (dist < eps_ + len)
+                    {
+                        inlierFoundLower = true;
+                    }
+                }
+                if (inlierFoundLower)
+                    lower_ -= 1.0;
+                if (inlierFoundUpper)
+                    upper_ -= 1.0;
+            }
+        }
     };
 
-    std::ostream &operator<<(std::ostream &out, const Box &box);
+    inline std::ostream &operator<<(std::ostream &out, const Box &box)
+    {
+        out << "min [" << box.min_.transpose() << "] max [" << box.max_.transpose()
+            << "] lower " << box.lower_ << " upper " << box.upper_;
+        return out;
+    }
 
     class BBTranslation
     {
@@ -95,7 +199,7 @@ namespace ars
         void setEps(const double eps);
 
         /**
-         * @brief Set max number of iteration before B&B alg stops 
+         * @brief Set max number of iteration before B&B alg stops
          */
         void setNumMaxIterations(const int nmi);
 
--- include/ars/ProcrustesUmeyama.h
+++ include/ars/ProcrustesUmeyama.h
@@ -14,129 +14,353 @@
 #define TWO_ 2
 #define THREE_ 3
 
-template<typename T>
-using  MatrixType = Eigen::Matrix<T,Eigen::Dynamic, Eigen::Dynamic>;
-   
-template<typename Scalar,int rank, typename sizeType>
-auto Tensor_to_Matrix(const Eigen::Tensor<Scalar,rank> &tensor,const sizeType rows,const sizeType cols)
+template <typename T>
+using MatrixType = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+
+template <typename Scalar, int rank, typename sizeType>
+auto Tensor_to_Matrix(const Eigen::Tensor<Scalar, rank> &tensor, const sizeType rows, const sizeType cols)
 {
-    return Eigen::Map<const MatrixType<Scalar>> (tensor.data(), rows,cols);
+    return Eigen::Map<const MatrixType<Scalar>>(tensor.data(), rows, cols);
 }
 
-
-template<typename Scalar, typename... Dims>
+template <typename Scalar, typename... Dims>
 auto Matrix_to_Tensor(const MatrixType<Scalar> &matrix, Dims... dims)
 {
-    constexpr int rank = sizeof... (Dims);
+    constexpr int rank = sizeof...(Dims);
     return Eigen::TensorMap<Eigen::Tensor<const Scalar, rank>>(matrix.data(), {dims...});
 }
 
+inline void procrustesUmeyama3d(Eigen::Affine3d &transfOut, const ars::VectorVector3 &cloudA, const ars::VectorVector3 &cloudB)
+{
+    transfOut = Eigen::Affine3d::Identity();
 
-void procrustesUmeyama3d(Eigen::Affine3d& transfOut, const ars::VectorVector3& cloudA, const ars::VectorVector3& cloudB);
+    ARS_ASSERT(cloudA.size() == cloudB.size());
 
-void procrustesUmeyama2d(Eigen::Affine2d& transfOut, const ars::VectorVector2& cloudA, const ars::VectorVector2& cloudB);
+    // Input clouds/matrices are supposed to have size m x n
+    int m = THREE_;
+    int n = std::min<int>(cloudA.size(), cloudB.size()); // TODO: fix when size(cloudA)!=size(cloudB)
 
+    // // cloudA->points.erase(cloudA->points.end() - 1);
+    // // cloudB->points.erase(cloudB->points.end() - 1);
+    // Eigen::MatrixXd clAMat = cloudA->getMatrixXdMap();
+    // Eigen::MatrixXd clBMat = cloudB->getMatrixXdMap();
+    // clAMat = clAMat.block<3,3>(0,0);
+    // clBMat = clBMat.block<3,3>(0,0);
+    Eigen::MatrixXd clAMat(m, n);
+    Eigen::MatrixXd clBMat(m, n);
 
-// function transf_out = procrustes_umeyama(a,b, dim)
-//     % Least squares estimation of transformation parameters between two
-//     % point patterns (clouds) a, b with dimensionality dim = 2 or 3
-//     % following procedure described in T-PAMI article by Shinji Umeyama, 1991
-    
-//     m = dim; % = size(a, 1)
-//     n = size(a,2); % = number of points
+    for (int i = 0; i < n; ++i)
+    {
+        Eigen::Vector3d ptA = cloudA[i];
+        Eigen::Vector3d ptB = cloudB[i];
+        clAMat.col(i) = ptA;
+        clBMat.col(i) = ptB;
+    }
 
-//     if dim == 2
-//         transf_out = rigidtform2d(0, [0,0]);
-//     elseif dim == 3
-//         transf_out = rigidtform3d([0.0, 0.0, 0.0], [0.0, 0.0, 0.0]);
-//     else
-//         disp("Error: dim has to be 2 or 3!");
-//         return;
-//     end
-    
+    std::cout << "m " << m << " n " << n << std::endl;
+    std::cout << "Mat a rows " << clAMat.rows() << " cols " << clAMat.cols() << std::endl;
+    std::cout << "clAMat" << std::endl
+              << clAMat << std::endl;
+    std::cout << "Mat b rows " << clBMat.rows() << " cols " << clBMat.cols() << std::endl;
+    std::cout << "clBMat" << std::endl
+              << clBMat << std::endl;
 
-//     %LEMMA
-//     %     [u,d,v] = svd(a * b');
-//     %     disp ("u")
-//     %     disp (u)
-//     %     disp ("d") %should be granted that diagonal elements are in decreasing order
-//     %     disp (d)
-//     %     disp ("v")
-//     %     disp (v) 
-//     %     
-//     %     s_min = eye(dim);
-//     %     if det(a * b') < 0 
-//     %         s_min(dim,dim) = -1;
-//     %     end
-//     %     
-//     %     rank_abt = rank(a*b');
-//     %     if rank_abt < m - 1
-//     %         disp("Error: rank(a*b') < dim - 1 -> returning eye transf");
-//     %         return;
-//     %     end
-//     % 
-//     %     R1 = eye(dim);
-//     % 
-//     %     if rank_abt == m-1
-//     %         s_argmin = eye(dim);
-//     %         if det(u) * det(v) == -1 
-//     %             s_argmin(dim,dim) = -1;
-//     %         end
-//     %         R1 = u * s_argmin * v';
-//     %     else %if rank_abt == m
-//     %         R1 = u * s_min * v';
-//     %     end
-//     % 
-//     %     disp("R1");
-//     %     disp(R1);
+    Eigen::MatrixXd svd1InputMat = clAMat * clBMat.transpose();
 
-//     %TODO: add computation of minimum value of mean squared error
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd1(svd1InputMat, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::MatrixXd u1 = svd1.matrixU();
+    Eigen::MatrixXd v1 = svd1.matrixV();
 
-//     a_centroid = mean(a,2); %mu_x
-//     b_centroid = mean(b,2); %mu_y
+    Eigen::Matrix3d s_min = Eigen::Matrix3d::Identity();
+    if (svd1InputMat.determinant() < 0)
+        s_min(2, 2) = -1;
 
-//     a_centroid_vec = repmat(a_centroid,1,n);
-//     diff_a = a - a_centroid_vec;
-//     a_sqnorm = (vecnorm(diff_a,2,1)).^2;    
-//     sigma_a = sum(a_sqnorm) / n; %sigma_x
+    // Note: for floating values matrices, you might get more accurate results with Eigen::ColPivHouseholderQR< MatrixType >
+    Eigen::ColPivHouseholderQR<Eigen::MatrixXd> luDecomp(svd1InputMat); // needed to compute rank
+    auto rank_abt = luDecomp.rank();
+    if ((int)rank_abt < m - 1)
+    {
+        std::cerr << "Error: rank(a*b') < dim - 1 -> returning eye transf" << std::endl;
+        // return;
+    }
 
-//     b_centroid_vec = repmat(b_centroid,1,n);
-//     diff_b = b - b_centroid_vec;
-//     b_sqnorm = (vecnorm(diff_b,2,1)).^2;    
-//     sigma_b = sum(b_sqnorm) / n; %sigma_y
+    Eigen::Matrix3d R1 = Eigen::Matrix3d::Identity();
+    if (rank_abt == m - 1)
+    {
+        Eigen::Matrix3d s_argmin = Eigen::Matrix3d::Identity();
+        if (u1.determinant() * v1.determinant() == -1)
+            s_argmin(2, 2) = -1;
+        R1 = u1 * s_argmin * v1.transpose();
+    }
+    else if (rank_abt == m)
+        R1 = u1 * s_min * v1.transpose();
 
-//     sigma_xy_complete = zeros(dim,dim,n); %Sigma_xy
-//     for ii = 1:n
-//         sigma_xy_complete(:,:,ii) = diff_b(:,ii) * diff_a(:,ii)';
-//     end
-//     sigma_xy = sum(sigma_xy_complete, 3) ./ n;
+    // TODO: add computation of minimum value of mean squared error
 
-//     [u_sigma_xy,d_sigma_xy,v_sigma_xy] = svd(sigma_xy);
+    Eigen::Vector3d a_centroid = clAMat.rowwise().mean(); // mu_x
+    Eigen::Vector3d b_centroid = clBMat.rowwise().mean(); // mu_y
 
-//     rank_sigma_xy = rank(sigma_xy)
-//     if rank_sigma_xy < m - 1
-//         disp("Error: rank(sigma_xy) < dim - 1 -> returning eye() transf");
-//         return;
-//     end
+    Eigen::MatrixXd a_centroid_repmat(a_centroid.rowwise().replicate(n));
+    Eigen::MatrixXd diff_a = clAMat - a_centroid_repmat;
+    Eigen::MatrixXd a_sqnorm = diff_a.colwise().squaredNorm();
+    double sigma_a = a_sqnorm.sum() / n; // sigma_x
 
-//     s_sigma_xy = eye(dim);
-//     if det(sigma_xy) < 0
-//         s_sigma_xy (dim,dim) = -1;
-//     end    
-//     if rank_sigma_xy == m-1
-//         if det(u_sigma_xy) * det(v_sigma_xy) == -1 
-//             s_sigma_xy(dim,dim) = -1;
-//         end
-//     end
-    
+    Eigen::MatrixXd b_centroid_repmat(b_centroid.rowwise().replicate(n));
+    Eigen::MatrixXd diff_b = clBMat - b_centroid_repmat;
+    Eigen::MatrixXd b_sqnorm = diff_b.colwise().squaredNorm();
+    double sigma_b = b_sqnorm.sum() / n; // sigma_y
 
-//     R2 = u_sigma_xy * s_sigma_xy * v_sigma_xy'
-//     c = trace(d_sigma_xy * s_sigma_xy) / sigma_a
-//     transl = b_centroid - c * R2 * a_centroid
-    
-//     transf_out.R = R2;
-//     transf_out.Translation = transl;
+    Eigen::Tensor<double, THREE_> sigma_xy_complete(m, m, n); // Sigma_xy
+    sigma_xy_complete.setZero();
+    for (int i = 0; i < n; ++i)
+    {
+        // MATLAB equivalent:
+        // sigma_xy_complete.block<dim,dim,n>(0,0,i) = diff_b.col(i) * diff_a.col(ii).transpose();
 
-// end
+        Eigen::Vector3d dAi = diff_a.col(i);
+        Eigen::Vector3d dBi = diff_b.col(i);
+        Eigen::MatrixXd tmp = dBi * dAi.transpose(); // this is actually a Matrix2d, but conversion function takes MatrixXd as input
 
-#endif /*CUDARS_PROCRUSTES_UMEYAMA_H_*/
+        Eigen::Tensor<double, TWO_> tmpTens = Matrix_to_Tensor(tmp, THREE_, THREE_);
+        sigma_xy_complete.chip(i, 2) = tmpTens;
+
+        // std::cout << "i " << i << std::endl;
+        // std::cout << "dAi " << std::endl
+        //           << dAi << std::endl;
+        // std::cout << "dBi " << std::endl
+        //           << dBi << std::endl;
+        // std::cout << "tmp " << std::endl
+        //           << tmp << std::endl;
+        // std::cout << "sigma_xy_complete.chip(i, 2) " << std::endl
+        //           << sigma_xy_complete.chip(i, 2) << std::endl;
+        // std::cout << "sigma_xy_complete " << std::endl
+        //           << sigma_xy_complete << std::endl;
+        // std::cout << "tmpTens " << std::endl
+        //           << tmpTens << std::endl;
+        // std::cout << std::endl;
+    }
+    // std::cout << "sigma_xy_complete " << std::endl
+    //           << sigma_xy_complete << std::endl;
+    Eigen::Tensor<double, 2> sigma_xy_tensor(THREE_, THREE_);
+    sigma_xy_tensor.setZero(); //!!
+    // built-in version
+    // std::cout << std::endl << "sigma_xy_tensor" << sigma_xy_tensor << std::endl;
+    // std::array<int, 3> two_dims{{3,3}};
+    // sigma_xy_tensor = sigma_xy_complete.sum(two_dims);
+    // hand-made version
+    for (int i = 0; i < n; ++i)
+    {
+        sigma_xy_tensor += sigma_xy_complete.chip(i, 2); // this still needs normalization!
+    }
+    // std::cout << "sigma_xy_tensor " << std::endl
+    //           << sigma_xy_tensor << std::endl; // this still needs normalization!
+    Eigen::MatrixXd sigma_xy = Tensor_to_Matrix(sigma_xy_tensor, m, m) / n;
+    // std::cout << "sigma_xy " << std::endl
+    //   << sigma_xy << std::endl;
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd_sigma_xy(sigma_xy, Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+    Eigen::FullPivLU<Eigen::Matrix3d> sigma_xy_decomp(sigma_xy); // needed to compute rank
+    auto rank_sigma_xy = sigma_xy_decomp.rank();
+    if ((int)rank_sigma_xy < m - 1)
+    {
+        std::cerr << "Error: rank(sigma_xy) < dim - 1 -> returning eye() transf" << std::endl;
+        return;
+    }
+
+    Eigen::MatrixXd u_sigma_xy = svd_sigma_xy.matrixU();
+    // Eigen::DiagonalMatrix<double, TWO_>(svd_sigma_xy.singularValues())
+    Eigen::MatrixXd d_sigma_xy = svd_sigma_xy.singularValues().asDiagonal(); // called S in Eigen documentation
+    Eigen::MatrixXd v_sigma_xy = svd_sigma_xy.matrixV();
+
+    Eigen::Matrix3d s_sigma_xy = Eigen::Matrix3d::Identity();
+    if (sigma_xy.determinant() < 0)
+        s_sigma_xy(1, 1) = -1;
+    if ((int)rank_sigma_xy == m - 1)
+        if (u_sigma_xy.determinant() * v_sigma_xy.determinant() == -1)
+            s_sigma_xy(1, 1) = -1;
+
+    Eigen::Matrix3d R2 = u_sigma_xy * s_sigma_xy * v_sigma_xy.transpose();
+    double c = (d_sigma_xy * s_sigma_xy).trace() / sigma_a;
+    Eigen::Vector3d transl = b_centroid - c * R2 * a_centroid;
+
+    transfOut.linear() = R2;
+    transfOut.translation() = transl;
+    transfOut.makeAffine();
+}
+
+inline void procrustesUmeyama2d(Eigen::Affine2d &transfOut, const ars::VectorVector2 &cloudA, const ars::VectorVector2 &cloudB)
+{
+    transfOut = Eigen::Affine2d::Identity();
+
+    ARS_ASSERT(cloudA.size() == cloudB.size());
+
+    // Input clouds/matrices are supposed to have size m x n
+    int m = TWO_;
+    int n = std::min<int>(cloudA.size(), cloudB.size()); // TODO: fix when size(cloudA)!=size(cloudB)
+
+    Eigen::MatrixXd clAMat(m, n);
+    Eigen::MatrixXd clBMat(m, n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        Eigen::Vector2d ptA = cloudA[i];
+        Eigen::Vector2d ptB = cloudB[i];
+        clAMat.col(i) = ptA;
+        clBMat.col(i) = ptB;
+    }
+
+    // std::cout << "m " << m << " n " << n << std::endl;
+    // std::cout << "Mat a rows " << clAMat.rows() << " cols " << clAMat.cols() << std::endl;
+    // std::cout << "clAMat" << std::endl
+    //           << clAMat << std::endl;
+    // std::cout << "Mat b rows " << clBMat.rows() << " cols " << clBMat.cols() << std::endl;
+    // std::cout << "clBMat" << std::endl
+    //           << clBMat << std::endl;
+
+    Eigen::MatrixXd svd1InputMat = clAMat * clBMat.transpose();
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd1(svd1InputMat, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::MatrixXd u1 = svd1.matrixU();
+    Eigen::MatrixXd v1 = svd1.matrixV();
+
+    Eigen::Matrix2d s_min = Eigen::Matrix2d::Identity();
+    if (svd1InputMat.determinant() < 0)
+        s_min(1, 1) = -1;
+
+    // Note: for floating values matrices, you might get more accurate results with Eigen::ColPivHouseholderQR< MatrixType >
+    Eigen::FullPivLU<Eigen::MatrixXd> luDecomp(svd1InputMat); // needed to compute rank
+    auto rank_abt = luDecomp.rank();
+    if ((int)rank_abt < m - 1)
+    {
+        std::cerr << "Error: rank(a*b') < dim - 1 -> returning eye transf" << std::endl;
+        return;
+    }
+
+    Eigen::Matrix2d R1 = Eigen::Matrix2d::Identity();
+    if (rank_abt == m - 1)
+    {
+        Eigen::Matrix2d s_argmin = Eigen::Matrix2d::Identity();
+        if (u1.determinant() * v1.determinant() == -1)
+            s_argmin(1, 1) = -1;
+        R1 = u1 * s_argmin * v1.transpose();
+    }
+    else if (rank_abt == m)
+        R1 = u1 * s_min * v1.transpose();
+
+    // TODO: add computation of minimum value of mean squared error
+
+    // std::cout << "m " << m << " n " << n << std::endl;
+    // std::cout << "Mat a rows " << clAMat.rows() << " cols " << clAMat.cols() << std::endl;
+
+    Eigen::Vector2d a_centroid = clAMat.rowwise().mean(); // mu_x
+    Eigen::Vector2d b_centroid = clBMat.rowwise().mean(); // mu_y
+    // std::cout << "a_centroid " << std::endl
+    //           << a_centroid << std::endl;
+
+    Eigen::MatrixXd a_centroid_repmat(a_centroid.rowwise().replicate(n));
+    // std::cout << "a_centroid_repmat rows " << a_centroid_repmat.rows() << " cols " << a_centroid_repmat.cols() << std::endl;
+    // std::cout << std::endl << "a_centroid_repmat " << std::endl << a_centroid_repmat << std::endl;
+    Eigen::MatrixXd diff_a = clAMat - a_centroid_repmat;
+    // std::cout << std::endl << "diff_a " << std::endl << diff_a << std::endl;
+    Eigen::MatrixXd a_sqnorm = diff_a.colwise().squaredNorm();
+    // std::cout << std::endl << "a_sqnorm " << std::endl << a_sqnorm << std::endl;
+    double sigma_a = a_sqnorm.sum() / n; // sigma_x
+    // std::cout << "sigma_a " << sigma_a << std::endl;
+
+    Eigen::MatrixXd b_centroid_repmat(b_centroid.rowwise().replicate(n));
+    // std::cout << "b_centroid_repmat rows " << b_centroid_repmat.rows() << " cols " << b_centroid_repmat.cols() << std::endl;
+    // std::cout << std::endl
+    //           << "b_centroid_repmat " << std::endl
+    //           << b_centroid_repmat << std::endl;
+    Eigen::MatrixXd diff_b = clBMat - b_centroid_repmat;
+    // std::cout << std::endl
+    //           << "diff_b " << std::endl
+    //           << diff_b << std::endl;
+    Eigen::MatrixXd b_sqnorm = diff_b.colwise().squaredNorm();
+    // std::cout << std::endl
+    //           << "b_sqnorm " << std::endl
+    //           << b_sqnorm << std::endl;
+    double sigma_b = b_sqnorm.sum() / n; // sigma_y
+    // std::cout << "sigma_b " << sigma_b << std::endl;
+
+    Eigen::Tensor<double, THREE_> sigma_xy_complete(m, m, n); // Sigma_xy
+    sigma_xy_complete.setZero();
+    for (int i = 0; i < n; ++i)
+    {
+        // MATLAB equivalent:
+        // sigma_xy_complete.block<dim,dim,n>(0,0,i) = diff_b.col(i) * diff_a.col(ii).transpose();
+
+        Eigen::Vector2d dAi = diff_a.col(i);
+        Eigen::Vector2d dBi = diff_b.col(i);
+        Eigen::MatrixXd tmp = dBi * dAi.transpose(); // this is actually a Matrix2d, but conversion function takes MatrixXd as input
+
+        Eigen::Tensor<double, TWO_> tmpTens = Matrix_to_Tensor(tmp, TWO_, TWO_);
+        sigma_xy_complete.chip(i, 2) = tmpTens;
+
+        // std::cout << "i " << i << std::endl;
+        // std::cout << "dAi " << std::endl
+        //           << dAi << std::endl;
+        // std::cout << "dBi " << std::endl
+        //           << dBi << std::endl;
+        // std::cout << "tmp " << std::endl
+        //           << tmp << std::endl;
+        // std::cout << "sigma_xy_complete.chip(i, 2) " << std::endl
+        //           << sigma_xy_complete.chip(i, 2) << std::endl;
+        // std::cout << "sigma_xy_complete " << std::endl
+        //           << sigma_xy_complete << std::endl;
+        // std::cout << "tmpTens " << std::endl
+        //           << tmpTens << std::endl;
+        // std::cout << std::endl;
+    }
+    // std::cout << "sigma_xy_complete " << std::endl
+    //           << sigma_xy_complete << std::endl;
+    Eigen::Tensor<double, 2> sigma_xy_tensor(TWO_, TWO_);
+    sigma_xy_tensor.setZero(); //!!
+    // built-in version
+    // std::cout << std::endl << "sigma_xy_tensor" << sigma_xy_tensor << std::endl;
+    // std::array<int, 3> two_dims{{2,2}};
+    // sigma_xy_tensor = sigma_xy_complete.sum(two_dims);
+    // hand-made version
+    for (int i = 0; i < n; ++i)
+    {
+        sigma_xy_tensor += sigma_xy_complete.chip(i, 2); // this still needs normalization!
+    }
+    // std::cout << "sigma_xy_tensor " << std::endl
+    //   << sigma_xy_tensor << std::endl; // this still needs normalization!
+    Eigen::MatrixXd sigma_xy = Tensor_to_Matrix(sigma_xy_tensor, m, m) / n;
+    // std::cout << "sigma_xy " << std::endl
+    //   << sigma_xy << std::endl;
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd_sigma_xy(sigma_xy, Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+    Eigen::FullPivLU<Eigen::Matrix2d> sigma_xy_decomp(sigma_xy); // needed to compute rank
+    auto rank_sigma_xy = sigma_xy_decomp.rank();
+    if ((int)rank_sigma_xy < m - 1)
+    {
+        std::cerr << "Error: rank(sigma_xy) < dim - 1 -> returning eye() transf" << std::endl;
+        return;
+    }
+
+    Eigen::MatrixXd u_sigma_xy = svd_sigma_xy.matrixU();
+    // Eigen::DiagonalMatrix<double, TWO_>(svd_sigma_xy.singularValues())
+    Eigen::MatrixXd d_sigma_xy = svd_sigma_xy.singularValues().asDiagonal(); // called S in Eigen documentation
+    Eigen::MatrixXd v_sigma_xy = svd_sigma_xy.matrixV();
+
+    Eigen::Matrix2d s_sigma_xy = Eigen::Matrix2d::Identity();
+    if (sigma_xy.determinant() < 0)
+        s_sigma_xy(1, 1) = -1;
+    if ((int)rank_sigma_xy == m - 1)
+        if (u_sigma_xy.determinant() * v_sigma_xy.determinant() == -1)
+            s_sigma_xy(1, 1) = -1;
+
+    Eigen::Matrix2d R2 = u_sigma_xy * s_sigma_xy * v_sigma_xy.transpose();
+    double c = (d_sigma_xy * s_sigma_xy).trace() / sigma_a;
+    Eigen::Vector2d transl = b_centroid - c * R2 * a_centroid;
+
+    transfOut.linear() = R2;
+    transfOut.translation() = transl;
+    transfOut.makeAffine();
+}
+
+#endif /*ARS_PROCRUSTES_UMEYAMA_H_*/
